3035cbc572217dad2627a7b0858489f87c2f20ac
rst_user_5 <kamilw2000@interia.eu>
2018-07-10 15:30:08 +0200

    Sose commit

---
+++ src/main/java/pl/jutupe/object/Talk.java
@@ -0,0 +1,37 @@
+package pl.jutupe.object;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Talk {
+
+    private JSONObject object;
+    private String title;
+    private Date date
+
+    public Talk() throws JSONException {
+        object = new JSONObject();
+
+        title = RandomStringUtils.randomAlphabetic(8) + " " + RandomStringUtils.randomAlphabetic(8);
+        object.put("title", title);
+        object.put("date", date);
+
+    }
+    public JSONObject getObject() {
+        return object;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return object.toString();
+    }
+}
--- src/test/java/pl/jutupe/TalkTests.java
+++ src/test/java/pl/jutupe/TalkTests.java
@@ -1,7 +1,14 @@
 package pl.jutupe;
 
+import org.junit.Test;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class TalkTests extends FunctionalTest {
+
+    @Test
+    public void  testPostTalk(){
+
+    }
 }
