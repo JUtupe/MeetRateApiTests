0d60e501977657f9f7e5eeeb4f4766e3eb9222a6
JUtupe <petryszyn@o2.pl>
2018-07-17 12:25:54 +0200

    + naprawa importu

--- src/test/java/pl/jutupe/QuestionTests.java
+++ src/test/java/pl/jutupe/QuestionTests.java
@@ -5,9 +5,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
-
+import pl.jutupe.enums.UserType;
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
 public class QuestionTests extends FunctionalTest {
 
